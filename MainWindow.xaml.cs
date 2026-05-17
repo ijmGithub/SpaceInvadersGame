aa7f119e5bb8e8000cee4568cd24d37ecb3ac938
ijm <ivan.jaraiz@gmail.com>
2026-05-17 17:28:46 +0200

    Added main window.

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,12 @@
+using System.Windows;
+
+namespace SpaceInvadersGame
+{
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
